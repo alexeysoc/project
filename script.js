d2c8ffd7b01eea2b9c1dca06049ccbd3aaa271b3
Alexey <alexeysoc@rambler.ru>
2020-04-25 18:35:48 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,60 @@
+'use strict'
+
+var number=5;
+var string='Ytllo!';
+var sym=Symbol();
+var boolean=true;
+null;
+undefined;
+var obj={};
+
+let lalala;
+console.log(lalala);
+
+let persone={
+    name: "John",
+    age: 25,
+    isMarried: false
+}
+
+console.log(persone["name"]);
+
+let arr=['plum.png', 'orange.jpg', 'apple']
+console.log(arr[0]);
+/*var leftBorderWidth=1;  
+let second=2;
+const pi=3.14;
+
+console.log(second);*/
+
+//alert('Hello Word');
+
+//let answer=confirm('Are you here?');
+//console.log(answer);
+
+//let answer=prompt("Есть ли вам 18?", "Да");
+//console.log(typeof(null));
+
+//console.log("arr" + " - object");
+//console.log(4 + " - object");
+let incr=10,
+    decr=10;
+
+//incr++;
+//decr--;
+console.log(incr++);
+console.log(decr--);
+
+console.log(5%2); //Остаток от деления
+
+console.log("2" ===2); //Остаток от деления
+
+let isChecket=true,
+    isClose=false;
+
+//console.log(isChecket && isClose ); // оператор И
+
+//console.log(isChecket || isClose ); // оператор ИЛИ
+
+
+console.log(isChecket || !isClose ); //! оператор отрицания
